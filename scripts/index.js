39a2b8f71791643bf21e03ca2c018f7aa7520a78
Vladimir Aksyonov <Kuggan@yandex.ru>
2020-10-28 18:04:00 +0500

    1-5 fixed notes

--- scripts/index.js
+++ scripts/index.js
@@ -4,21 +4,24 @@ const editButton = document.querySelector('.profile__button-edit');
 const profileName = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__subtitle');
 const form = document.querySelector('.popup__form');
-const fieldName = document.querySelector('.popup__input_name');
-const fieldDescription = document.querySelector('.popup__input_description');
+const fieldName = document.querySelector('.popup__input_enter_name');
+const fieldDescription = document.querySelector('.popup__input_enter_description');
 
 function showPopup() {
-popup.classList.add('popup_opened');
-}
-function closePopup() {
-popup.classList.remove('popup_opened');
-}
-editButton.addEventListener('click', showPopup);
-popupCloseButton.addEventListener('click', closePopup);
+  popup.classList.add('popup_opened');
+  fieldName.value = profileName.textContent;
+  fieldDescription.value = profileDescription.textContent;
 
+} 
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 function submitForm(event){
   event.preventDefault();
   profileName.textContent = fieldName.value;
   profileDescription.textContent = fieldDescription.value;
+  closePopup();
 }
+editButton.addEventListener('click', showPopup);
+popupCloseButton.addEventListener('click', closePopup);
 form.addEventListener('submit',submitForm);
