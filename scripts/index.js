c5aa535ed335a073be22c2cd35abc1ca3aad1e55
Vladimir Aksyonov <Kuggan@yandex.ru>
2020-11-27 17:22:45 +0500

    1-1 validation

--- scripts/index.js
+++ scripts/index.js
@@ -19,25 +19,57 @@ const popupImageZoomed = document.querySelector('.popup__image')
 const closePopupImage = document.querySelector('.popup__close_image');
 const cards = document.querySelector('.elements');
 const cardElementTemplate = document.querySelector('.element-template').content;
-// create a function to open popup.
+
+// create a function to open a popup.
 function showPopup(popup){
   popup.classList.add('popup_opened');
+  document.addEventListener('keydown', closePopupEscape);
+  document.addEventListener('mousedown', (evt)=>{
+    if (evt.target.classList.contains('popup')){
+      closePopup(popup)
+    }
+  });
   
 }
+// create a function to close a popup
 function closePopup(popup){
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupEscape);
+ 
 }
+// create a function to close a popup by 'Escape'
+function closePopupEscape(evt){
+  const popupImage = document.querySelector('.popup_image');
+  if (evt.key === 'Escape'){
+    closePopup(popupImage);
+  }
+  const popupAddCard = document.querySelector('.popup_add-card');
+  if (evt.key === 'Escape'){
+    closePopup(popupAddCard);
+  }
+  const popupEditProfile = document.querySelector('.popup_edit-profile')
+  if (evt.key === 'Escape'){
+    closePopup(popupEditProfile);
+  }
+  
+}
+
 // event handlers
  editButton.addEventListener('click', ()=>{ 
    showPopup(popupEditProfile);
    fieldName.value = profileName.textContent;
    fieldDescription.value = profileDescription.textContent;
-  });
+   resetValidationState(popupEditProfile, validationConfig);
+  } );
+ 
  closeEditButton.addEventListener('click', () => closePopup(popupEditProfile));
- addCardButton.addEventListener('click', ()=>{ 
+
+  addCardButton.addEventListener('click', ()=>{ 
    showPopup(popupAddCard);
-   popupFormAddCard.reset(); 
+   popupFormAddCard.reset();
+   resetValidationState(popupAddCard, validationConfig);
   });
+  
  closeAddCardButton.addEventListener('click', ()=> closePopup(popupAddCard));
  // submit changes of profile a name and a description
  formEditProfile.addEventListener('submit',event=>{
@@ -45,6 +77,7 @@ function closePopup(popup){
   profileName.textContent = fieldName.value;
   profileDescription.textContent = fieldDescription.value;
   closePopup(popupEditProfile);
+  
  });
  // open popup of zoomed picture
 function showPopupImage(src, alt){
@@ -95,6 +128,7 @@ function showPopupImage(src, alt){
      };
      addCard(dataCard);
      closePopup(popupAddCard)
+     
 });
 
   initialCards.forEach(addCard);
